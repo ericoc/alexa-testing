f8b3bfbe678ebfc1e7550481762af98f2c2ff156
Eric OC <eric@ericoc.com>
2017-03-12 11:57:39 -0400

    comment cleanup

--- fruit.php
+++ fruit.php
@@ -6,12 +6,13 @@ Right now I have this set up so that when I say "alexa fruit", it responds with 
 
 */
 
+// Require the file with the AlexaOut function that builds a JSON response
 require_once('alexa.func.php');
 
 // The output is always JSON
 header('Content-Type: application/json;charset=UTF-8');
 
-// Create an array of fruits and pick one of the fruits to respond with
+// Create an array of fruits, randomize (shuffle) it, pick the first item, then create and print a friendly phrase using JSON
 $fruits = array('pineapples', 'apples', 'oranges', 'mangoes', 'pears', 'tomatoes', 'strawberries', 'bananas', 'peaches', 'grapes', 'cherries', 'grapefruits');
 shuffle($fruits);
 $fruit = $fruits[0];
