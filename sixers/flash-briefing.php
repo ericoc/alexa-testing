c363fcb66f44c99f74c5b1b07498087aaf726a4e
Eric OC <eric@ericoc.com>
2017-09-02 13:52:09 -0400

    update for 2017-18 season

--- sixers/flash-briefing.php
+++ sixers/flash-briefing.php
@@ -1,147 +1,708 @@
 <?php
 
-// Create an array of arrays of all the games ($games!)
 $games = array(
 
-	'20170312' => array(
-		'opponent' => 'Los Angeles Lakers',
-		'when' => '20170312T213000',
-		'stadium' => 'Staples Center',
-		'city' => 'Los Angeles',
-		'state' => 'CA'
-	),
-
-	'20170314' => array(
-		'opponent' => 'Golden State Warriors',
-		'when' => '20170314T223000',
-		'stadium' => 'Oracle Arena',
-		'city' => 'Oakland',
-		'state' => 'CA'
-	),
-
-	'20170317' => array(
-		'opponent' => 'Dallas Mavericks',
-		'when' => '20170317T190000',
-		'stadium' => 'Wells Fargo Center',
+	'20171004' => array(
+		'opponent' => 'Memphis Grizzlies',
+		'when' => '20171004T190000',
+		'location' => 'Wells Fargo Center',
 		'city' => 'Philadelphia',
 		'state' => 'PA'
 	),
 
-	'20170319' => array(
+	'20171006' => array(
 		'opponent' => 'Boston Celtics',
-		'when' => '20170319T130000',
-		'stadium' => 'Wells Fargo Center',
+		'when' => '20171006T190000',
+		'location' => 'Wells Fargo Center',
 		'city' => 'Philadelphia',
 		'state' => 'PA'
 	),
 
-	'20170320' => array(
-		'opponent' => 'Orlando Magic',
-		'when' => '20170320T190000',
-		'stadium' => 'Amway Center',
-		'city' => 'Orlando',
-		'state' => 'FL'
+	'20171009' => array(
+		'opponent' => 'Boston Celtics',
+		'when' => '20171009T193000',
+		'location' => 'TD Garden',
+		'city' => 'Boston',
+		'state' => 'MA'
 	),
 
-	'20170322' => array(
-		'opponent' => 'Oklahoma City Thunder',
-		'when' => '20170322T200000',
-		'stadium' => 'Chesapeake Energy Arena',
-		'city' => 'Oklahoma City',
-		'state' => 'OK'
-	),
-
-	'20170324' => array(
-		'opponent' => 'Chicago Bulls',
-		'when' => '20170324T200000',
-		'stadium' => 'United Center',
-		'city' => 'Chicago',
-		'state' => 'IL'
-	),
-
-	'20170326' => array(
-		'opponent' => 'Indiana Pacers',
-		'when' => '20170326T180000',
-		'stadium' => 'Bankers Life Fieldhouse',
-		'city' => 'Indianapolis',
-		'state' => 'IN'
-	),
-
-	'20170328' => array(
+	'20171011' => array(
 		'opponent' => 'Brooklyn Nets',
-		'when' => '20170328T193000',
-		'stadium' => 'Barclays Center',
-		'city' => 'Brooklyn',
+		'when' => '20171011T193000',
+		'location' => 'Nassau Veterans Memorial Coliseum',
+		'city' => 'Uniondale',
 		'state' => 'NY'
 	),
 
-	'20170329' => array(
-		'opponent' => 'Atlanta Hawks',
-		'when' => '20170329T190000',
-		'stadium' => 'Wells Fargo Center',
+	'20171013' => array(
+		'opponent' => 'Miami Heat',
+		'when' => '20171013T200000',
+		'location' => 'Sprint Center',
+		'city' => 'Kansas City',
+		'state' => 'MO'
+	),
+
+	'20171018' => array(
+		'opponent' => 'Washington Wizards',
+		'when' => '20171018T190000',
+		'location' => 'Capital One Arena',
+		'city' => 'Washington',
+		'state' => 'DC'
+	),
+
+	'20171020' => array(
+		'opponent' => 'Boston Celtics',
+		'when' => '20171020T190000',
+		'location' => 'Wells Fargo Center',
 		'city' => 'Philadelphia',
 		'state' => 'PA'
 	),
 
-	'20170331' => array(
-		'opponent' => 'Cleveland Cavaliers',
-		'when' => '20170331T193000',
-		'stadium' => 'Quicken Loans Arena',
-		'city' => 'Cleveland',
-		'state' => 'OH'
-	),
-
-	'20170402' => array(
+	'20171021' => array(
 		'opponent' => 'Toronto Raptors',
-		'when' => '20170402T180000',
-		'stadium' => 'Air Canada Centre',
+		'when' => '20171021T193000',
+		'location' => 'Air Canada Centre',
 		'city' => 'Toronto',
 		'state' => 'ON'
 	),
 
-	'20170404' => array(
-		'opponent' => 'Brooklyn Nets',
-		'when' => '20170404T190000',
-		'stadium' => 'Wells Fargo Center',
+	'20171023' => array(
+		'opponent' => 'Detroit Pistons',
+		'when' => '20171023T190000',
+		'location' => 'Little Caesars Arena',
+		'city' => 'Detroit',
+		'state' => 'MI'
+	),
+
+	'20171025' => array(
+		'opponent' => 'Houston Rockets',
+		'when' => '20171025T190000',
+		'location' => 'Wells Fargo Center',
 		'city' => 'Philadelphia',
 		'state' => 'PA'
 	),
 
-	'20170406' => array(
-		'opponent' => 'Chicago Bulls',
-		'when' => '20170406T190000',
-		'stadium' => 'Wells Fargo Center',
+	'20171028' => array(
+		'opponent' => 'Dallas Mavericks',
+		'when' => '20171028T203000',
+		'location' => 'American Airlines Center',
+		'city' => 'Dallas',
+		'state' => 'TX'
+	),
+
+	'20171030' => array(
+		'opponent' => 'Houston Rockets',
+		'when' => '20171030T200000',
+		'location' => 'Toyota Center',
+		'city' => 'Houston',
+		'state' => 'TX'
+	),
+
+	'20171101' => array(
+		'opponent' => 'Atlanta Hawks',
+		'when' => '20171101T190000',
+		'location' => 'Wells Fargo Center',
 		'city' => 'Philadelphia',
 		'state' => 'PA'
 	),
 
-	'20170408' => array(
-		'opponent' => 'Milwaukee Bucks',
-		'when' => '20170408T190000',
-		'stadium' => 'Wells Fargo Center',
-		'city' => 'Philadelphia',
-		'state' => 'PA'
-	),
-
-	'20170410' => array(
+	'20171103' => array(
 		'opponent' => 'Indiana Pacers',
-		'when' => '20170410T190000',
-		'stadium' => 'Wells Fargo Center',
+		'when' => '20171103T190000',
+		'location' => 'Wells Fargo Center',
 		'city' => 'Philadelphia',
 		'state' => 'PA'
 	),
 
-	'20170412' => array(
+	'20171107' => array(
+		'opponent' => 'Utah Jazz',
+		'when' => '20171107T210000',
+		'location' => 'Vivint Smart Home Arena',
+		'city' => 'Salt Lake City',
+		'state' => 'UT'
+	),
+
+	'20171109' => array(
+		'opponent' => 'Sacramento Kings',
+		'when' => '20171109T220000',
+		'location' => 'Golden 1 Center',
+		'city' => 'Sacramento',
+		'state' => 'CA'
+	),
+
+	'20171111' => array(
+		'opponent' => 'Golden State Warriors',
+		'when' => '20171111T203000',
+		'location' => 'ORACLE Arena',
+		'city' => 'Oakland',
+		'state' => 'CA'
+	),
+
+	'20171113' => array(
+		'opponent' => 'LA Clippers',
+		'when' => '20171113T223000',
+		'location' => 'STAPLES Center',
+		'city' => 'Los Angeles',
+		'state' => 'CA'
+	),
+
+	'20171115' => array(
+		'opponent' => 'Los Angeles Lakers',
+		'when' => '20171115T223000',
+		'location' => 'STAPLES Center',
+		'city' => 'Los Angeles',
+		'state' => 'CA'
+	),
+
+	'20171118' => array(
+		'opponent' => 'Golden State Warriors',
+		'when' => '20171118T193000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171120' => array(
+		'opponent' => 'Utah Jazz',
+		'when' => '20171120T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171122' => array(
+		'opponent' => 'Portland Trail Blazers',
+		'when' => '20171122T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171125' => array(
+		'opponent' => 'Orlando Magic',
+		'when' => '20171125T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171127' => array(
+		'opponent' => 'Cleveland Cavaliers',
+		'when' => '20171127T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171129' => array(
+		'opponent' => 'Washington Wizards',
+		'when' => '20171129T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171130' => array(
+		'opponent' => 'Boston Celtics',
+		'when' => '20171130T193000',
+		'location' => 'TD Garden',
+		'city' => 'Boston',
+		'state' => 'MA'
+	),
+
+	'20171202' => array(
+		'opponent' => 'Detroit Pistons',
+		'when' => '20171202T193000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171204' => array(
+		'opponent' => 'Phoenix Suns',
+		'when' => '20171204T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171207' => array(
+		'opponent' => 'Los Angeles Lakers',
+		'when' => '20171207T200000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171209' => array(
+		'opponent' => 'Cleveland Cavaliers',
+		'when' => '20171209T200000',
+		'location' => 'Quicken Loans Arena',
+		'city' => 'Cleveland',
+		'state' => 'OH'
+	),
+
+	'20171210' => array(
+		'opponent' => 'New Orleans Pelicans',
+		'when' => '20171210T190000',
+		'location' => 'Smoothie King Center',
+		'city' => 'New Orleans',
+		'state' => 'LA'
+	),
+
+	'20171212' => array(
+		'opponent' => 'Minnesota Timberwolves',
+		'when' => '20171212T200000',
+		'location' => 'Target Center',
+		'city' => 'Minneapolis',
+		'state' => 'MN'
+	),
+
+	'20171215' => array(
+		'opponent' => 'Oklahoma City Thunder',
+		'when' => '20171215T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171218' => array(
+		'opponent' => 'Chicago Bulls',
+		'when' => '20171218T200000',
+		'location' => 'United Center',
+		'city' => 'Chicago',
+		'state' => 'IL'
+	),
+
+	'20171219' => array(
+		'opponent' => 'Sacramento Kings',
+		'when' => '20171219T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171221' => array(
+		'opponent' => 'Toronto Raptors',
+		'when' => '20171221T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20171223' => array(
+		'opponent' => 'Toronto Raptors',
+		'when' => '20171223T170000',
+		'location' => 'Air Canada Centre',
+		'city' => 'Toronto',
+		'state' => 'ON'
+	),
+
+	'20171225' => array(
 		'opponent' => 'New York Knicks',
-		'when' => '20170412T200000',
-		'stadium' => 'Madison Square Garden',
+		'when' => '20171225T120000',
+		'location' => 'Madison Square Garden',
 		'city' => 'New York',
 		'state' => 'NY'
+	),
+
+	'20171228' => array(
+		'opponent' => 'Portland Trail Blazers',
+		'when' => '20171228T223000',
+		'location' => 'Moda Center',
+		'city' => 'Portland',
+		'state' => 'OR'
+	),
+
+	'20171230' => array(
+		'opponent' => 'Denver Nuggets',
+		'when' => '20171230T210000',
+		'location' => 'Pepsi Center',
+		'city' => 'Denver',
+		'state' => 'CO'
+	),
+
+	'20171231' => array(
+		'opponent' => 'Phoenix Suns',
+		'when' => '20171231T200000',
+		'location' => 'Talking Stick Resort Arena',
+		'city' => 'Phoenix',
+		'state' => 'AZ'
+	),
+
+	'20180103' => array(
+		'opponent' => 'San Antonio Spurs',
+		'when' => '20180103T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180105' => array(
+		'opponent' => 'Detroit Pistons',
+		'when' => '20180105T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180111' => array(
+		'opponent' => 'Boston Celtics',
+		'when' => '20180111T150000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180115' => array(
+		'opponent' => 'Toronto Raptors',
+		'when' => '20180115T130000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180118' => array(
+		'opponent' => 'Boston Celtics',
+		'when' => '20180118T190000',
+		'location' => 'TD Garden',
+		'city' => 'Boston',
+		'state' => 'MA'
+	),
+
+	'20180120' => array(
+		'opponent' => 'Milwaukee Bucks',
+		'when' => '20180120T193000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180122' => array(
+		'opponent' => 'Memphis Grizzlies',
+		'when' => '20180122T200000',
+		'location' => 'FedExForum',
+		'city' => 'Memphis',
+		'state' => 'TN'
+	),
+
+	'20180124' => array(
+		'opponent' => 'Chicago Bulls',
+		'when' => '20180124T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180126' => array(
+		'opponent' => 'San Antonio Spurs',
+		'when' => '20180126T203000',
+		'location' => 'AT&T Center',
+		'city' => 'San Antonio',
+		'state' => 'TX'
+	),
+
+	'20180128' => array(
+		'opponent' => 'Oklahoma City Thunder',
+		'when' => '20180128T180000',
+		'location' => 'Chesapeake Energy Arena',
+		'city' => 'Oklahoma City',
+		'state' => 'OK'
+	),
+
+	'20180129' => array(
+		'opponent' => 'Milwaukee Bucks',
+		'when' => '20180129T200000',
+		'location' => 'BMO Harris Bradley Center',
+		'city' => 'Milwaukee',
+		'state' => 'WI'
+	),
+
+	'20180131' => array(
+		'opponent' => 'Brooklyn Nets',
+		'when' => '20180131T193000',
+		'location' => 'Barclays Center',
+		'city' => 'Brooklyn',
+		'state' => 'NY'
+	),
+
+	'20180202' => array(
+		'opponent' => 'Miami Heat',
+		'when' => '20180202T200000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180203' => array(
+		'opponent' => 'Indiana Pacers',
+		'when' => '20180203T190000',
+		'location' => 'Bankers Life Fieldhouse',
+		'city' => 'Indianapolis',
+		'state' => 'IN'
+	),
+
+	'20180206' => array(
+		'opponent' => 'Washington Wizards',
+		'when' => '20180206T200000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180209' => array(
+		'opponent' => 'New Orleans Pelicans',
+		'when' => '20180209T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180210' => array(
+		'opponent' => 'LA Clippers',
+		'when' => '20180210T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180212' => array(
+		'opponent' => 'New York Knicks',
+		'when' => '20180212T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180214' => array(
+		'opponent' => 'Miami Heat',
+		'when' => '20180214T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180222' => array(
+		'opponent' => 'Chicago Bulls',
+		'when' => '20180222T200000',
+		'location' => 'United Center',
+		'city' => 'Chicago',
+		'state' => 'IL'
+	),
+
+	'20180224' => array(
+		'opponent' => 'Orlando Magic',
+		'when' => '20180224T170000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180225' => array(
+		'opponent' => 'Washington Wizards',
+		'when' => '20180225T200000',
+		'location' => 'Capital One Arena',
+		'city' => 'Washington',
+		'state' => 'DC'
+	),
+
+	'20180227' => array(
+		'opponent' => 'Miami Heat',
+		'when' => '20180227T193000',
+		'location' => 'AmericanAirlines Arena',
+		'city' => 'Miami',
+		'state' => 'FL'
+	),
+
+	'20180301' => array(
+		'opponent' => 'Cleveland Cavaliers',
+		'when' => '20180301T200000',
+		'location' => 'Quicken Loans Arena',
+		'city' => 'Cleveland',
+		'state' => 'OH'
+	),
+
+	'20180302' => array(
+		'opponent' => 'Charlotte Hornets',
+		'when' => '20180302T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180304' => array(
+		'opponent' => 'Milwaukee Bucks',
+		'when' => '20180304T200000',
+		'location' => 'BMO Harris Bradley Center',
+		'city' => 'Milwaukee',
+		'state' => 'WI'
+	),
+
+	'20180306' => array(
+		'opponent' => 'Charlotte Hornets',
+		'when' => '20180306T190000',
+		'location' => 'Spectrum Center',
+		'city' => 'Charlotte',
+		'state' => 'NC'
+	),
+
+	'20180308' => array(
+		'opponent' => 'Miami Heat',
+		'when' => '20180308T193000',
+		'location' => 'AmericanAirlines Arena',
+		'city' => 'Miami',
+		'state' => 'FL'
+	),
+
+	'20180311' => array(
+		'opponent' => 'Brooklyn Nets',
+		'when' => '20180311T193000',
+		'location' => 'Barclays Center',
+		'city' => 'Brooklyn',
+		'state' => 'NY'
+	),
+
+	'20180313' => array(
+		'opponent' => 'Indiana Pacers',
+		'when' => '20180313T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180315' => array(
+		'opponent' => 'New York Knicks',
+		'when' => '20180315T193000',
+		'location' => 'Madison Square Garden',
+		'city' => 'New York',
+		'state' => 'NY'
+	),
+
+	'20180316' => array(
+		'opponent' => 'Brooklyn Nets',
+		'when' => '20180316T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180319' => array(
+		'opponent' => 'Charlotte Hornets',
+		'when' => '20180319T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180321' => array(
+		'opponent' => 'Memphis Grizzlies',
+		'when' => '20180321T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180322' => array(
+		'opponent' => 'Orlando Magic',
+		'when' => '20180322T190000',
+		'location' => 'Amway Center',
+		'city' => 'Orlando',
+		'state' => 'FL'
+	),
+
+	'20180324' => array(
+		'opponent' => 'Minnesota Timberwolves',
+		'when' => '20180324T180000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180326' => array(
+		'opponent' => 'Denver Nuggets',
+		'when' => '20180326T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180328' => array(
+		'opponent' => 'New York Knicks',
+		'when' => '20180328T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180330' => array(
+		'opponent' => 'Atlanta Hawks',
+		'when' => '20180330T193000',
+		'location' => 'Philips Arena',
+		'city' => 'Atlanta',
+		'state' => 'GA'
+	),
+
+	'20180401' => array(
+		'opponent' => 'Charlotte Hornets',
+		'when' => '20180401T130000',
+		'location' => 'Spectrum Center',
+		'city' => 'Charlotte',
+		'state' => 'NC'
+	),
+
+	'20180403' => array(
+		'opponent' => 'Brooklyn Nets',
+		'when' => '20180403T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180404' => array(
+		'opponent' => 'Detroit Pistons',
+		'when' => '20180404T190000',
+		'location' => 'Little Caesars Arena',
+		'city' => 'Detroit',
+		'state' => 'MI'
+	),
+
+	'20180406' => array(
+		'opponent' => 'Cleveland Cavaliers',
+		'when' => '20180406T190000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180408' => array(
+		'opponent' => 'Dallas Mavericks',
+		'when' => '20180408T130000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
+	),
+
+	'20180410' => array(
+		'opponent' => 'Atlanta Hawks',
+		'when' => '20180410T193000',
+		'location' => 'Philips Arena',
+		'city' => 'Atlanta',
+		'state' => 'GA'
+	),
+
+	'20180411' => array(
+		'opponent' => 'Milwaukee Bucks',
+		'when' => '20180411T200000',
+		'location' => 'Wells Fargo Center',
+		'city' => 'Philadelphia',
+		'state' => 'PA'
 	)
+
 );
 
+
 // Define a function that will create a friendly phrase to describe a game
-function createPhrase ($game) {
+function create_phrase ($game) {
 
 	// Start a friendly phrase to respond with
 	$phrase = 'The ';
@@ -163,7 +724,7 @@ function createPhrase ($game) {
 }
 
 // Define a function to create JSON for Alexa Flash Briefing skills
-function BriefingOut ($uid, $update_date, $title_text, $main_text, $redirect_url = null) {
+function briefing_out ($uid, $update_date, $title_text, $main_text, $redirect_url = null) {
 
 	// Create the short array to return for Flash Briefing skills, JSON encode it, and return it
 	$final = array('uid' => $uid, 'updateDate' => $update_date, 'titleText' => $title_text, 'mainText' => $main_text, 'redirectUrl' => null);
@@ -183,7 +744,7 @@ $title = 'Philadelphia 76ers Games (Flash Briefing)';
 if ( (isset($games["$today"])) && (!empty($games["$today"])) ) {
 
 	// Create a friendly phrase with the details of the game today
-	$phrase = createPhrase($games["$today"]);
+	$phrase = create_phrase($games["$today"]);
 
 // If there is no game today, set the phrase accordingly
 } else {
@@ -192,5 +753,5 @@ if ( (isset($games["$today"])) && (!empty($games["$today"])) ) {
 
 // Finally, generate and return the JSON
 header('Content-Type: application/json;charset=UTF-8');
-$out = BriefingOut($uid, $update_date, $title, $phrase);
+$out = briefing_out($uid, $update_date, $title, $phrase);
 echo $out;
